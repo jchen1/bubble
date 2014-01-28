1e37bf893e5384a9fd4ebffd3ed468ce3629c541
Rolando Schneiderman <rschneid@andrew.cmu.edu>
2014-01-28 15:09:20 -0500

    added bluetooth network code and fucked up the only view we use

--- Bubble/ViewController.m
+++ Bubble/ViewController.m
@@ -8,6 +8,21 @@
 
 #import "ViewController.h"
 #import "MyScene.h"
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
+
+
+@interface ViewController ()<MCBrowserViewControllerDelegate, MCSessionDelegate, UITextFieldDelegate>
+
+@property (nonatomic, strong) MCBrowserViewController *browserVC;
+@property (nonatomic, strong) MCAdvertiserAssistant *advertiser;
+@property (nonatomic, strong) MCSession *mySession;
+@property (nonatomic, strong) MCPeerID *myPeerID;
+
+@property (nonatomic, strong) UIButton *browseButton;
+@property (nonatomic, strong) UITextView *textBox;
+@property (nonatomic, strong) UITextField *chatBox;
+
+@end
 
 @implementation ViewController
 
@@ -19,6 +34,10 @@
     SKView * skView = (SKView *)self.view;
     skView.showsFPS = YES;
     skView.showsNodeCount = YES;
+    [self setUpMultipeer];
+    [self setUpUI];
+
+
     
     // Create and configure the scene.
     SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
@@ -48,4 +67,120 @@
     // Release any cached data, images, etc that aren't in use.
 }
 
+- (void) setUpUI{
+    //  Setup the browse button
+    self.browseButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.browseButton setTitle:@"Bluetooth" forState:UIControlStateNormal];
+    self.browseButton.frame = CGRectMake(130, 20, 60, 30);
+    [self.view addSubview:self.browseButton];
+    
+    //  Setup TextBox
+    self.textBox = [[UITextView alloc] initWithFrame: CGRectMake(40, 150, 140, 270)];
+    self.textBox.editable = NO;
+    self.textBox.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview: self.textBox];
+    
+    //  Setup ChatBox
+    self.chatBox = [[UITextField alloc] initWithFrame: CGRectMake(40, 60, 140, 70)];
+    self.chatBox.backgroundColor = [UIColor lightGrayColor];
+    self.chatBox.returnKeyType = UIReturnKeySend;
+    [self.view addSubview:self.chatBox];
+    [self.browseButton addTarget:self action:@selector(showBrowserVC) forControlEvents:UIControlEventTouchUpInside];
+    self.chatBox.delegate = self;
+}
+- (void) setUpMultipeer{
+    //  Setup peer ID
+    self.myPeerID = [[MCPeerID alloc] initWithDisplayName:[UIDevice currentDevice].name];
+    
+    //  Setup session
+    self.mySession = [[MCSession alloc] initWithPeer:self.myPeerID];
+    
+    //  Setup BrowserViewController
+    self.browserVC = [[MCBrowserViewController alloc] initWithServiceType:@"chat" session:self.mySession];
+    
+    //  Setup Advertiser
+    self.advertiser = [[MCAdvertiserAssistant alloc] initWithServiceType:@"chat" discoveryInfo:nil session:self.mySession];
+    self.browserVC.delegate = self;
+    [self.advertiser start];
+    self.mySession.delegate = self;
+}
+
+- (void) showBrowserVC{
+    [self presentViewController:self.browserVC animated:YES completion:nil];
+}
+
+- (void) dismissBrowserVC{
+    [self.browserVC dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma marks MCBrowserViewControllerDelegate
+
+// Notifies the delegate, when the user taps the done button
+- (void)browserViewControllerDidFinish:(MCBrowserViewController *)browserViewController{
+    [self dismissBrowserVC];
+}
+
+// Notifies delegate that the user taps the cancel button.
+- (void)browserViewControllerWasCancelled:(MCBrowserViewController *)browserViewController{
+    [self dismissBrowserVC];
+}
+
+- (void) receiveMessage: (NSString *) message fromPeer: (MCPeerID *) peer{
+    //  Create the final text to append
+    NSString *finalText;
+    if (peer == self.myPeerID) {
+        finalText = [NSString stringWithFormat:@"\nme: %@\n", message];
+    }
+    else{
+        finalText = [NSString stringWithFormat:@"\n%@: %@\n", peer.displayName, message];
+    }
+    
+    //  Append text to text box
+    self.textBox.text = [self.textBox.text stringByAppendingString:finalText];
+}
+- (void) sendText{
+    //  Retrieve text from chat box and clear chat box
+    NSString *message = self.chatBox.text;
+    self.chatBox.text = @"";
+    
+    //  Convert text to NSData
+    NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
+    
+    //  Send data to connected peers
+    NSError *error;
+    [self.mySession sendData:data toPeers:[self.mySession connectedPeers] withMode:MCSessionSendDataUnreliable error:&error];
+    
+    //  Append your own text to text box
+    [self receiveMessage: message fromPeer: self.myPeerID];
+}
+- (void)session:(MCSession *)session peer:(MCPeerID *)peerID didChangeState:(MCSessionState)state{
+    
+}
+
+// Received data from remote peer
+- (void)session:(MCSession *)session didReceiveData:(NSData *)data fromPeer:(MCPeerID *)peerID{
+    //  Decode data back to NSString
+    NSString *message = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    //  append message to text box on main thread
+    dispatch_async(dispatch_get_main_queue(),^{
+        [self receiveMessage: message fromPeer: peerID];
+    });
+}
+
+// Received a byte stream from remote peer
+- (void)session:(MCSession *)session didReceiveStream:(NSInputStream *)stream withName:(NSString *)streamName fromPeer:(MCPeerID *)peerID{
+    
+}
+
+// Start receiving a resource from remote peer
+- (void)session:(MCSession *)session didStartReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID withProgress:(NSProgress *)progress{
+    
+}
+
+// Finished receiving a resource from remote peer and saved the content in a temporary location - the app is responsible for moving the file to a permanent location within its sandbox
+- (void)session:(MCSession *)session didFinishReceivingResourceWithName:(NSString *)resourceName fromPeer:(MCPeerID *)peerID atURL:(NSURL *)localURL withError:(NSError *)error{
+    
+}
 @end
+
