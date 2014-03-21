fdef883892e129081c106d20359003262b396685
Stephen Greco <sgreco94@verizon.net>
2014-03-21 17:14:23 -0400

    finished twitter

--- Bubble/SplashViewController.m
+++ Bubble/SplashViewController.m
@@ -112,7 +112,22 @@
 }
 
 - (IBAction)twitterView {
-    
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+    {
+        SLComposeViewController *tweetSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [tweetSheet setInitialText:@"Check out this game! http://t.co/pAHoAtMxZ0 #bubbleOutlast"];
+        [self presentViewController:tweetSheet animated:YES completion:nil];
+    }
+    else
+    {
+        UIAlertView *alertView = [[UIAlertView alloc]
+                                  initWithTitle:@"Twitter Unavailable"
+                                  message:@"Please sign in and make sure you have an Internet connection."
+                                  delegate:self
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil];
+        [alertView show];
+    }
 }
 
 - (IBAction)gameCenterView{
